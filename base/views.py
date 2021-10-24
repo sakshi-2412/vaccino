f4c35f9af662d035bc6e9a13e85941693deb036f
sakshi <sakshinch@gmail.com>
2021-10-24 20:58:02 +0530

    add notif page

--- base/views.py
+++ base/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def notif(request):
+    return render(request, 'notif.html')
