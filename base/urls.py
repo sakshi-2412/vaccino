6f975a7b18ad5b0bcb7569a4d57d37756cd45a48
sakshi <sakshinch@gmail.com>
2021-10-31 18:07:48 +0530

    add dashboard

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
    path('', views.index, name='index'),
    path('notif/', views.notif, name='notif'),
    path('profile/', views.profile, name='profile'),
+   path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- base/views.py
+++ base/views.py
@@ -8,4 +8,41 @@ def notif(request):
     return render(request, 'notif.html')
 
 def profile(request):
-    return render(request, 'profile.html')    
+    return render(request, 'profile.html') 
+
+def dashboard(request):
+    p1labels=["Yes","No"]
+    p1data=[100,20]
+
+    p2labels=["Yes","No"]
+    p2data=[70,50]
+
+    p3labels=["Yes","No"]
+    p3data=[50,70]
+
+    p4labels=["Covishield","Covaxin","Sputnik"]
+    p4data=[50,40,10]
+
+    b1labels=["Year 1","Year 2","Year 3"]
+    bar1_d1=[30,20,50]
+    bar1_d2=[10,20,40]
+
+    context = {
+        'p1labels': p1labels,
+        'p1data': p1data,
+
+        'p2labels': p2labels,
+        'p2data': p2data,
+
+        'p3labels': p3labels,
+        'p3data': p3data,
+
+        'p4labels': p4labels,
+        'p4data': p4data,
+
+        'b1labels': p4labels,
+        'bar1_d1': bar1_d1,
+        'bar1_d2': bar1_d2,
+
+    }
+    return render(request, 'dashboard.html', context=context)    
