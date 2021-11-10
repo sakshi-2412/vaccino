bca7a9b437dcfe97316da1f01ae92d2b32a1568c
Debarati Bandopadhyay <85282947+debarati-06@users.noreply.github.com>
2021-11-10 17:26:11 +0530

    Database connection (#13)
    * Icons for register page
    
    * Add login and logout page
    
    * Vaccination Details
    
    * Delete pdfs
    
    * Test Account
    
    * Edit button
    
    * Database connection

--- Vaccino/settings.py
+++ Vaccino/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Vaccino.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vaccino_database',
+        'USER': 'dbadmin',
+        'PASSWORD': 'pinkblue',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
